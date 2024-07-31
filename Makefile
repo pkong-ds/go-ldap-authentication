5f55df235e50d07ce738f0191a48c9ae751f9baa
yyorkchan <yyorkchan@gmail.com>
2024-07-31 11:21:48 +0800

    Make ldap client use environment variable

---
+++ Makefile
@@ -0,0 +1,14 @@
+include .env
+
+# LDAP_URL=ldap://localhost:389
+# LDAP_BASE_DN = cn=dataadmin,ou=datateam,dc=example,dc=org
+# LDAP_BIND_DN = cn=admin,dc=example,dc=org
+# LDAP_BIND_PASSWORD = admin
+.PHONY: start
+start:
+	LDAP_URL=$(LDAP_URL) \
+	LDAP_BASE_DN=$(LDAP_BASE_DN) \
+	LDAP_BIND_DN=$(LDAP_BIND_DN) \
+	LDAP_BIND_PASSWORD=$(LDAP_BIND_PASSWORD) \
+	LDAP_SEARCH_QUERY=$(LDAP_SEARCH_QUERY) \
+	go run main.go
--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"crypto/tls"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/go-ldap/ldap/v3"
 )
@@ -11,38 +13,36 @@ import (
 type LDAPClient struct {
 	conn *ldap.Conn
 
-	host string
-	port int
-	bindDN string
+	URL          string
+	baseDN       string
+	bindDN       string
 	bindPassword string
-
-	username string
-	password string
+	searchQuery  string
 }
 
-func NewLDAPClient(host string, port int, bindDN string, bindPassword string, username string, password string) *LDAPClient {
+func NewLDAPClient() *LDAPClient {
 	return &LDAPClient{
-		host: host,
-		port: port,
-		bindDN: bindDN,
-		bindPassword: bindPassword,
-
-		username: username,
-		password: password,
+		URL:          os.Getenv("LDAP_URL"),
+		baseDN:       os.Getenv("LDAP_BASE_DN"),
+		bindDN:       os.Getenv("LDAP_BIND_DN"),
+		bindPassword: os.Getenv("LDAP_BIND_PASSWORD"),
+		searchQuery:  os.Getenv("LDAP_SEARCH_QUERY"),
 	}
 }
 
-func (l *LDAPClient) Connect() (error) {
-	conn, err := ldap.DialURL(fmt.Sprintf("%s:%d", l.host, l.port))
+func (l *LDAPClient) Connect() error {
+	conn, err := ldap.DialURL(l.URL)
 	if err != nil {
 		log.Fatalf("Failed to connect to LDAP server: %s", err)
 		return err
 	}
 
 	l.conn = conn
+	return nil
+}
 
-	// Bind to the LDAP server with read access
-	err = l.conn.Bind(l.bindDN, l.bindPassword)
+func (l *LDAPClient) Bind() error {
+	err := l.conn.Bind(l.bindDN, l.bindPassword)
 	if err != nil {
 		log.Fatalf("Failed to bind to LDAP server: %s", err)
 		return err
@@ -51,8 +51,8 @@ func (l *LDAPClient) Connect() (error) {
 	return nil
 }
 
-func (l *LDAPClient) StartTLSConnect() (error) {
-	conn, err := ldap.DialURL(fmt.Sprintf("%s:%d", l.host, l.port))
+func (l *LDAPClient) StartTLSConnect() error {
+	conn, err := ldap.DialURL(l.URL)
 	if err != nil {
 		log.Fatalf("Failed to connect to LDAP server: %s", err)
 		return err
@@ -66,27 +66,20 @@ func (l *LDAPClient) StartTLSConnect() (error) {
 		return err
 	}
 
-	// Bind to the LDAP server with read access
-	err = l.conn.Bind(l.bindDN, l.bindPassword)
-	if err != nil {
-		log.Fatalf("Failed to bind to LDAP server: %s", err)
-		return err
-	}
-
 	return nil
 }
-
 
 func (l *LDAPClient) Close() {
 	l.conn.Close()
 }
 
-func (l *LDAPClient) AuthenticateUser() (error) {
+func (l *LDAPClient) AuthenticateUser(username string, password string) error {
+	fmt.Println(l.searchQuery)
 	// Search for the user
 	searchRequest := ldap.NewSearchRequest(
-		"cn=dataadmin,ou=datateam,dc=example,dc=org",
+		l.baseDN,
 		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
-		fmt.Sprintf("(%v)", &ldap.AttributeTypeAndValue{Type: "uid", Value: l.username}),
+		fmt.Sprintf("(%v)", &ldap.AttributeTypeAndValue{Type: l.searchQuery, Value: username}),
 		[]string{},
 		nil,
 	)
@@ -97,36 +90,66 @@ func (l *LDAPClient) AuthenticateUser() (error) {
 	}
 
 	if len(sr.Entries) != 1 {
-		return fmt.Errorf("User not found or too many entries returned")
+		return fmt.Errorf("user not found or too many entries returned")
 	}
 
 	userDN := sr.Entries[0].DN
 
 	// Bind as the user to verify their password
-	err = l.conn.Bind(userDN, l.password)
+	err = l.conn.Bind(userDN, password)
 	if err != nil {
-		return fmt.Errorf("Failed to authenticate user: %s", err)
+		return fmt.Errorf("failed to authenticate user: %s", err)
 	}
 
 	return nil
 }
 
 func main() {
+	// get command line input for username and password
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Enter username: ")
+	scanner.Scan()
+	username := scanner.Text()
+	fmt.Print("Enter password: ")
+	scanner.Scan()
+	password := scanner.Text()
 
+	var err error
 	// Create a new LDAP client
-	client := NewLDAPClient("ldap://localhost", 389, "cn=admin,dc=example,dc=org", "admin", "chrislee", "123")
-	// client := NewLDAPClient("ldaps://localhost", 636, "cn=admin,dc=example,dc=org", "admin", "chrislee", "123")
+	client := NewLDAPClient()
+	// Check if url starts with ldap://
+	if client.URL[:7] == "ldap://" {
+		// Connect to the LDAP server
+		err = client.StartTLSConnect()
+		if err != nil {
+			log.Printf("Failed to connect: %s", err)
+			err = client.Connect()
+			if err != nil {
+				log.Fatalf("Failed to connect: %s", err)
+			} else {
+				fmt.Println("Connected")
+			}
+		}
+	} else if client.URL[:8] == "ldaps://" {
+		// Connect to the LDAP server
+		err = client.Connect()
+		if err != nil {
+			log.Fatalf("Failed to connect: %s", err)
+		} else {
+			fmt.Println("Connected")
+		}
+	}
 
-	// Connect to the LDAP server
-	err := client.StartTLSConnect()
+	// Bind to the LDAP server
+	err = client.Bind()
 	if err != nil {
-		log.Fatalf("Failed to connect: %s", err)
+		log.Fatalf("Failed to bind: %s", err)
 	} else {
-		fmt.Println("Connected")
+		fmt.Println("Bound")
 	}
 
 	// Authenticate the user
-	err = client.AuthenticateUser()
+	err = client.AuthenticateUser(username, password)
 	if err != nil {
 		log.Fatalf("Failed to authenticate user: %s", err)
 	} else {
@@ -136,3 +159,23 @@ func main() {
 	// Close the connection
 	client.Close()
 }
+
+/*
+docker run --name ldap-server \
+        --hostname ldap-server \
+				--volume /Users/york/Desktop/Oursky/certs:/container/service/slapd/assets/certs \
+				--env LDAP_TLS_CRT_FILENAME=my-ldap.crt \
+				--env LDAP_TLS_KEY_FILENAME=my-ldap.key \
+				--env LDAP_TLS_CA_CRT_FILENAME=the-ca.crt \
+				--env LDAP_TLS_VERIFY_CLIENT="try" \
+		-p 389:389 -p 636:636 \
+		--detach \
+		osixia/openldap:latest
+
+docker run --name ldap-admin \
+    -p 6443:443 \
+    --link ldap-server:ldap-host \
+    --env PHPLDAPADMIN_LDAP_HOSTS=ldap-host \
+    --detach \
+    osixia/phpldapadmin:latest
+*/
